c71325b343a4e612857f1da54fff013fd40dd5bf
DYNOH812 <munyuadennis@gmail.com>
2025-02-25 16:07:02 +0300

    used material tab navigator to sort out orders

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -5,6 +5,7 @@ export default function MenuStack () {
     return (
     <Stack>
       <Stack.Screen name="index" options={{title:'Orders'}}/>
+      <Stack.Screen name="list" options={{headerShown:false}}/>
     </Stack>
     )
 }
